2b2013fc8aa7f7dbd8f79824f32c624d8a6ff413
Vaibhav Sharma <sharmavaibhav581@gmail.com>
2025-02-08 15:25:58 +0530

    app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,23 @@
 import './App.css'
 import MovieCard from './components/MovieCard'
 import Home from './pages/Home'
+import { Routes, Route } from 'react-router-dom'
+import NavBar from './components/NavBar'
 
 function App() {
 
   return (
-    <>
-    {/* <MovieCard movie={{title: "IT", release_date:"2024"}}/> */}
-    <Home/>
-    </>
+    <div >
+      <NavBar/>
+
+      <main className='main-content'>
+
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/favourites' element={<favourites/>}/>
+    </Routes>
+   </main>
+    </div>
   )
 }
 
