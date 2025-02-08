c963703ab09f9e4c6e8c67a5a3fa7abda93c99d3
Vaibhav Sharma <sharmavaibhav581@gmail.com>
2025-02-08 15:25:12 +0530

    fav movies

---
+++ frontend/src/pages/Favourites.jsx
@@ -0,0 +1,14 @@
+function Favourites(){
+
+    return
+    <>
+    
+    <div className="favourites-empty">
+        <h2>No Favourite movies yet</h2>
+        <p>start adding movies to your favourites</p>
+    </div>
+
+    </>
+}
+
+export default Favourites
