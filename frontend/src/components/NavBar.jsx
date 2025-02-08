ad768fd4d0f4de3e3667131eb4c45ff3d0841618
Vaibhav Sharma <sharmavaibhav581@gmail.com>
2025-02-08 15:23:37 +0530

    Navbar

---
+++ frontend/src/components/NavBar.jsx
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom"
+
+function NavBar(){
+
+    return
+    <>
+     <nav className="navbar">
+        <div className="navbar-brand">
+
+            <Link to="/">Movie App</Link>
+        </div>
+
+        <div className="navbar-links">
+            <Link to="/" className="nav-link">Home</Link>
+            <Link to="/favourites" className="nav-link">favourites</Link>
+        </div>
+     </nav>
+    </>
+}
+
+export default NavBar
