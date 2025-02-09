5438156228fc660bf76a3a040bb869920a42d664
Vaibhav Sharma <sharmavaibhav581@gmail.com>
2025-02-09 16:10:56 +0530

    NAVBAR

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,21 +1,16 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
+import "C:/Users/HP/OneDrive/Desktop/MovieRecs/frontend/src/css/Navbar.css"
 
-function NavBar(){
-
-    return
-    <>
-     <nav className="navbar">
+function NavBar() {
+    return <nav className="navbar">
         <div className="navbar-brand">
-
             <Link to="/">Movie App</Link>
         </div>
-
         <div className="navbar-links">
             <Link to="/" className="nav-link">Home</Link>
-            <Link to="/favourites" className="nav-link">favourites</Link>
+            <Link to="/favorites" className="nav-link">Favorites</Link>
         </div>
-     </nav>
-    </>
+    </nav>
 }
 
 export default NavBar
