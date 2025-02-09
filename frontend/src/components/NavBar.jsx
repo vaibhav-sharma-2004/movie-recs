3497b117e77164f5e6a5aa0220dc086b5bae0cfe
Vaibhav Sharma <sharmavaibhav581@gmail.com>
2025-02-09 16:49:28 +0530

    NAVBAR

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import "C:/Users/HP/OneDrive/Desktop/MovieRecs/frontend/src/css/Navbar.css"
+import "../css/NavBar.css"
 
 function NavBar() {
     return <nav className="navbar">
