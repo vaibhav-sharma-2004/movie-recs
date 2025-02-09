52a9126ca731b48da251592c88070964415d4db9
Vaibhav Sharma <sharmavaibhav581@gmail.com>
2025-02-09 16:49:58 +0530

    App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,9 @@
 import "./css/App.css";
 import Favorites from "./pages/Favorites";
-import Home from "C:/Users/HP/OneDrive/Desktop/MovieRecs/frontend/src/pages/Home.jsx";
+import Home from "./pages/Home";
+
 import { Routes, Route } from "react-router-dom";
-import { MovieProvider } from "C:/Users/HP/OneDrive/Desktop/MovieRecs/frontend/src/contexts/MovieContext.jsx";
+import { MovieProvider } from "./contexts/MovieContext";
 import NavBar from "./components/NavBar";
 
 function App() {
