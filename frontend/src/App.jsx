53bd1a6b638770384c0949a59519f49ee8773fed
Vaibhav Sharma <sharmavaibhav581@gmail.com>
2025-02-09 16:17:38 +0530

    add context

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,22 @@
-import './App.css'
-import MovieCard from './components/MovieCard'
-import Home from './pages/Home'
-import { Routes, Route } from 'react-router-dom'
-import NavBar from './components/NavBar'
+import "./css/App.css";
+import Favorites from "./pages/Favorites";
+import Home from "C:/Users/HP/OneDrive/Desktop/MovieRecs/frontend/src/pages/Home.jsx";
+import { Routes, Route } from "react-router-dom";
+import { MovieProvider } from "C:/Users/HP/OneDrive/Desktop/MovieRecs/frontend/src/contexts/MovieContext.jsx";
+import NavBar from "./components/NavBar";
 
 function App() {
-
   return (
-    <div >
-      <NavBar/>
-
-      <main className='main-content'>
-
-    <Routes>
-      <Route path='/' element={<Home/>}/>
-      <Route path='/favourites' element={<favourites/>}/>
-    </Routes>
-   </main>
-    </div>
-  )
+    <MovieProvider>
+      <NavBar />
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/favorites" element={<Favorites />} />
+        </Routes>
+      </main>
+    </MovieProvider>
+  );
 }
 
-export default App
+export default App;
